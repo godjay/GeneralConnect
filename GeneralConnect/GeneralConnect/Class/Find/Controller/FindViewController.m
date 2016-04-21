3e11a05a6ceea59b0c72bed6e866d06eaa0b9520
lirenjie <lirenjie@appledeMac-mini.local>
2016-04-21 15:56:16 +0800

    ok

--- GeneralConnect/GeneralConnect/Class/Find/Controller/FindViewController.m
+++ GeneralConnect/GeneralConnect/Class/Find/Controller/FindViewController.m
@@ -9,27 +9,80 @@
 #import "FindViewController.h"
 #import "SearchViewController.h"
 #import "RJNavViewController.h"
+#import "plusView.h"
+#import "FireViewController.h"
 
 @interface FindViewController ()
 @property (weak,nonatomic)UIView *screenView;
 @property (weak,nonatomic)UIButton *selectedBtn;
+@property (weak,nonatomic)UIView *maskView;
+@property (weak,nonatomic)UIView *plusView;
 @end
 
 @implementation FindViewController
 
+- (void)viewWillAppear:(BOOL)animated{
+    self.screenView.hidden = NO; //设置筛选视图显示
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor colorWithRed:245/255.0 green:245/255.0 blue:247/255.0 alpha:1.0];
-
+    
     //创建导航栏右搜索按钮
     [self setupSearchView];
 
     //创建筛选视图
     [self setupScreenView];
     
+    //创建加号视图
+    [self setupPlusView];
+
+    //接收通知，弹出加号视图或发动态控制器
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(presentTextPlus) name:@"presentPlus" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fireNew) name:@"fire" object:nil];
+
 }
 
+//初始化加号视图
+- (void)setupPlusView{
+    [self setupMaskView];
+    
+    plusView *myplusView = [[NSBundle mainBundle] loadNibNamed:@"plusView" owner:self options:nil].lastObject;
+    myplusView.frame = CGRectMake(0, self.tabBarController.view.bounds.size.height, self.view.frame.size.width, 200);
+//    [self.view insertSubview:myplusView aboveSubview:self.maskView];
+    [self.tabBarController.view addSubview:myplusView];
+    self.plusView = myplusView;
+}
+
+//遮罩视图
+- (void)setupMaskView{
+    UIView *maskView = [[UIView alloc] initWithFrame:self.view.frame];
+//    self.tabBarController.view.userInteractionEnabled = NO;
+    maskView.backgroundColor = [UIColor whiteColor];
+    [self.tabBarController.view addSubview:maskView];
+    maskView.hidden = YES;
+    self.maskView = maskView;
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(maskViewClick)];
+    [self setupMaskBgView];
+    [_maskView addGestureRecognizer:tap];
+}
+
+- (void)setupMaskBgView{
+    UIImageView *bgView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"text"]];
+    bgView.frame = self.maskView.frame;
+    [self.maskView addSubview:bgView];
+    
+    //取消按钮
+    UIButton *Xbtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    Xbtn.frame = CGRectMake(0, self.view.frame.size.height - 50, self.view.frame.size.width, 50);
+    [Xbtn setImage:[UIImage imageNamed:@"×"] forState:UIControlStateNormal];
+    [Xbtn setImage:[UIImage imageNamed:@"x1.jpg"] forState:UIControlStateHighlighted];
+    [Xbtn addTarget:self action:@selector(maskViewClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.maskView addSubview:Xbtn];
+}
+
+//创建筛选视图
 - (void)setupScreenView{
     NSArray *screenBtnArray = @[@"全国",@"智能排序",@"筛选"];
     UIView *screenView = [[UIView alloc] initWithFrame:CGRectMake(0, 64, self.view.frame.size.width, 40)];
@@ -58,6 +111,7 @@
     
 }
 
+//创建导航栏右搜索按钮
 - (void)setupSearchView{
     UIButton *searchBtn = [UIButton buttonWithType:UIButtonTypeCustom];
     searchBtn.frame = CGRectMake(0, 0, 22, 20);
@@ -79,6 +133,7 @@
     return 0;
 }
 
+//筛选按钮点击事件
 - (void)screenBtnAction:(UIButton *)btn{
     self.selectedBtn.selected = NO;
     btn.selected = YES;
@@ -87,9 +142,38 @@
 
 //搜索按钮点击
 - (void)searchBtnAction{
+    self.screenView.hidden = YES;
     SearchViewController *searchVc = [[SearchViewController alloc] init];
     searchVc.hidesBottomBarWhenPushed = YES;
     [self.navigationController pushViewController:searchVc animated:YES];
 }
 
+
+/*通知方法*/
+//动画弹出加号按钮视图
+- (void)presentTextPlus{
+    self.maskView.hidden = NO;
+    self.tabBarController.tabBar.hidden = YES;
+    [UIView animateWithDuration:0.3 animations:^{
+        self.plusView.transform = CGAffineTransformMakeTranslation(0, -(self.plusView.frame.size.height + self.tabBarController.tabBar.frame.size.height));
+    } completion:nil];
+}
+
+//跳转发布控制器
+- (void)fireNew{
+    [self maskViewClick];
+    FireViewController *fireVc = [[FireViewController alloc] init];
+    RJNavViewController *nav = [[RJNavViewController alloc] initWithRootViewController:fireVc];
+    [self presentViewController:nav animated:YES completion:nil];
+}
+
+//隐藏加号视图
+- (void)maskViewClick{
+    self.tabBarController.tabBar.hidden = NO;
+    [UIView animateWithDuration:0.3 delay:0.0 options:0 animations:^{
+        self.plusView.transform = CGAffineTransformIdentity;
+    } completion:^(BOOL finished) {
+        self.maskView.hidden = YES;
+    }];
+}
 @end
--- GeneralConnect/GeneralConnect/Class/Main/Controller/RJTabBarController.m
+++ GeneralConnect/GeneralConnect/Class/Main/Controller/RJTabBarController.m
@@ -13,13 +13,10 @@
 #import "MineViewController.h"
 #import "RJTarBar.h"
 #import "RJNavViewController.h"
-#import "plusView.h"
-#import "FireViewController.h"
 
 @interface RJTabBarController ()
 @property (weak,nonatomic)RJTarBar *customTabar;
-@property (weak,nonatomic)UIView *maskView;
-@property (weak,nonatomic)UIView *plusView;
+
 
 @end
 
@@ -32,12 +29,8 @@
 
     [self setupChildVc];
     
-    [self setupPlusView];
-    
-    //接收通知
+    //接收通知---切换控制器
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(chooseController:) name:@"indexChange" object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(presentTextPlus) name:@"presentPlus" object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fireNew) name:@"fire" object:nil];
 }
 
 - (void)viewWillAppear:(BOOL)animated{
@@ -50,44 +43,6 @@
     }
 }
 
-//初始化加号视图
-- (void)setupPlusView{
-    [self setupMaskView];
-    
-    plusView *myplusView = [[NSBundle mainBundle] loadNibNamed:@"plusView" owner:self options:nil].lastObject;
-    myplusView.frame = CGRectMake(0, self.view.bounds.size.height, self.view.frame.size.width, 200);
-    [self.view insertSubview:myplusView aboveSubview:self.maskView];
-    self.plusView = myplusView;
-}
-
-//遮罩视图
-- (void)setupMaskView{
-    UIView *maskView = [[UIView alloc] initWithFrame:self.view.frame];
-    self.tabBarController.view.userInteractionEnabled = NO;
-    maskView.backgroundColor = [UIColor whiteColor];
-//    [self.view addSubview:maskView];
-//    [self.view insertSubview:maskView aboveSubview:self.tabBar];
-    [self.view addSubview:maskView];
-    maskView.hidden = YES;
-    self.maskView = maskView;
-    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(maskViewClick)];
-    [self setupMaskBgView];
-    [_maskView addGestureRecognizer:tap];
-}
-
-- (void)setupMaskBgView{
-    UIImageView *bgView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"text"]];
-    bgView.frame = self.maskView.frame;
-    [self.maskView addSubview:bgView];
-    
-    //取消按钮
-    UIButton *Xbtn = [UIButton buttonWithType:UIButtonTypeCustom];
-    Xbtn.frame = CGRectMake(0, self.view.frame.size.height - 50, self.view.frame.size.width, 50);
-    [Xbtn setImage:[UIImage imageNamed:@"×"] forState:UIControlStateNormal];
-    [Xbtn setImage:[UIImage imageNamed:@"x1.jpg"] forState:UIControlStateHighlighted];
-    [Xbtn addTarget:self action:@selector(maskViewClick) forControlEvents:UIControlEventTouchUpInside];
-    [self.maskView addSubview:Xbtn];
-}
 
 - (void)setupTarBar{
     RJTarBar *customTabar = [[RJTarBar alloc] init];
@@ -115,34 +70,10 @@
 }
 
 /*通知方法*/
-
-//动画弹出加号按钮视图
-- (void)presentTextPlus{
-    self.maskView.hidden = NO;
-    [UIView animateWithDuration:0.3 animations:^{
-        self.plusView.transform = CGAffineTransformMakeTranslation(0, -(self.plusView.frame.size.height + self.tabBar.frame.size.height));
-    } completion:nil];
-}
-
 //切换控制器
 - (void)chooseController:(NSNotification *)not{
     self.selectedIndex = [not.object integerValue];
 }
 
-//跳转发布控制器
-- (void)fireNew{
-    [self maskViewClick];
-    FireViewController *fireVc = [[FireViewController alloc] init];
-    RJNavViewController *nav = [[RJNavViewController alloc] initWithRootViewController:fireVc];
-    [self presentViewController:nav animated:YES completion:nil];
-}
 
-//隐藏加号视图
-- (void)maskViewClick{
-    [UIView animateWithDuration:0.3 delay:0.0 options:0 animations:^{
-        self.plusView.transform = CGAffineTransformIdentity;
-    } completion:^(BOOL finished) {
-        self.maskView.hidden = YES;
-    }];
-}
 @end
